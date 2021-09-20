e5fb64ede48eb10d96165445f040df47c124f018
Daniel-Muruthi <daniel.muruthi@student.moringaschool.com>
2021-09-20 09:04:19 +0300

    This will initialize the application and various extensions

---
+++ app/__init__.py
@@ -0,0 +1,27 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+from flask import Flask
+from config import config_options
+
+bootstrap = Bootstrap()
+db = SQLAlchemy
+
+def create_app(config_name):
+    # Initializing application
+    app = Flask(__name__,template_folder='templates')
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    # setting config
+    from .requests import configure_request
+    configure_request(app)
+
+    return app
---
+++ config.py
@@ -0,0 +1,29 @@
+class Config:
+    '''
+    General configuration parent class
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql + psycopg2://moringa:Wiseman1995@localhost/pitch'
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
